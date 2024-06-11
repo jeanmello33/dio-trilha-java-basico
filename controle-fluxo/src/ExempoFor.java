c5f5e5de941a3014dfd2ba10e76f1d9b2bbc908d
Jean Mello <jean.mello9@gmail.com>
2024-06-11 14:03:57 -0300

    Estruturas de Repetição em Java

---
+++ controle-fluxo/src/ExempoFor.java
@@ -0,0 +1,11 @@
+public class ExempoFor {
+
+    public static void main(String[] args) {
+        
+        for(int carneirinhos = 1 ; carneirinhos <=20; carneirinhos ++) {
+			System.out.println(carneirinhos + " - Carneirinho(s)");
+		}
+
+        System.out.println("Joãozinho dormiu");
+    }    
+}
